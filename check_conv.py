6b2e81a9f4e94975f409a8cea49b4a6c8c9e11d8
Ruchira-R <44061338+Ruchira-R@users.noreply.github.com>
2022-08-14 17:02:51 +0530

    Updated check_conv

--- check_conv.py
+++ check_conv.py
@@ -4,15 +4,15 @@ count=0
 n=0
 conv =0.5 #this value will vary for different test cases in the backend
 def rewrite_pagerank():
-	os.remove("/home/nitish/hadoop/pagerank/v")
+	os.remove("/home/ruchvad/hadoop/pagerank/v")
 
-	source = "/home/nitish/hadoop/pagerank/v1"
-	destination = "/home/nitish/hadoop/pagerank/v"
+	source = "/home/ruchvad/hadoop/pagerank/v1"
+	destination = "/home/ruchvad/hadoop/pagerank/v"
 	dest = shutil.copyfile(source, destination) 
 
 
 
-with open("/home/nitish/hadoop/pagerank/v") as file1, open("/home/nitish/hadoop/pagerank/v1") as file2:
+with open("/home/ruchvad/hadoop/pagerank/v") as file1, open("/home/ruchvad/hadoop/pagerank/v1") as file2:
 	for line1, line2 in zip(file1, file2):
 		count+=1
 		old_pagerank=float(line1.split(",")[1])
